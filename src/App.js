7785cc787f4d2e3712789b66a42876add45afdbc
Jardel Cunha <jardelgcunha.ti@gmail.com>
2024-01-19 09:37:32 -0300

    Creating routes
    - Refactoring.

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
-import SobreMim from "./paginas/SobreMim";
-import Inicio from "./paginas/Inicio";
-
-console.log(window.location);
-
-const pagina = window.location.pathname === '/' ? <Inicio /> : <SobreMim />
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Inicio from './paginas/Inicio'
+import SobreMim from './paginas/SobreMim'
 
 function App() {
-  return pagina;
-}
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Inicio />} />
+        <Route path="/sobremim" element={<SobreMim />} />
+        <Route path="*" element={<div>Página não encontrada</div>} />
+      </Routes>
+    </BrowserRouter>)
+};
 
 export default App;
