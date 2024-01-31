fb291cbacb640a4800e2e75a3eab799e1a28094b
Jardel Cunha <jardelgcunha.ti@gmail.com>
2024-01-31 05:38:54 -0300

    Treating post not found

--- src/paginas/Post/index.js
+++ src/paginas/Post/index.js
@@ -1,8 +1,10 @@
 import './Post.css';
-import { useParams } from "react-router-dom"
+import { Route, Routes, useParams } from "react-router-dom"
 import posts from 'json/posts.json';
 import PostModelo from "componentes/PostModelo";
 import ReactMarkdown from "react-markdown";
+import NaoEncontrada from 'paginas/NaoEncontrada';
+import PaginaPadrao from 'paginas/PaginaPadrao';
 
 export default function Post() {
     const parametros = useParams();
@@ -12,21 +14,27 @@ export default function Post() {
     });
 
     if (!post) {
-        return <h1>Post não encontrado</h1>
+        return <NaoEncontrada />
     }
 
     console.log(post);
 
     return (
-        <PostModelo
-            fotoCapa={`/assets/posts/${post.id}/capa.png`}
-            titulo={post.titulo}
-        >
-            <div className="post-markdown-container">
-                <ReactMarkdown>
-                    {post.texto}
-                </ReactMarkdown>
-            </div>
-        </PostModelo>
+        <Routes>
+            <Route path='*' element={<PaginaPadrao />}>
+                <Route index element={
+                    <PostModelo
+                        fotoCapa={`/assets/posts/${post.id}/capa.png`}
+                        titulo={post.titulo}
+                    >
+                        <div className="post-markdown-container">
+                            <ReactMarkdown>
+                                {post.texto}
+                            </ReactMarkdown>
+                        </div>
+                    </PostModelo>
+                } />
+            </Route>
+        </Routes>
     )
 }
