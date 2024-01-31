85bd4e2898ad7ce999bec78f842fd9d8610561ac
Jardel Cunha <jardelgcunha.ti@gmail.com>
2024-01-30 23:59:36 -0300

    Creating PostModelo component
    Styling PostModelo and SobreMim.

--- src/paginas/SobreMim/index.js
+++ src/paginas/SobreMim/index.js
@@ -1,5 +1,24 @@
+import styles from './SobreMim.module.css'
+
+import PostModelo from "componentes/PostModelo";
+import fotoCapa from 'assets/sobre_mim_capa.png';
+import fotoSobreMim from 'assets/sobre_mim_foto.png';
+
 export default function SobreMim() {
     return (
-        <h1>Sobre mim...</h1>
+        <PostModelo
+            fotoCapa={fotoCapa}
+            titulo="Sobre mim"
+        >
+            <h3 className={styles.subtitulo}>
+                Olá, eu sou  o Antônio
+            </h3>
+
+            <img
+                src={fotoSobreMim}
+                alt='Foto do Antônio sorrindo'
+                className={styles.fotoSobreMim}
+            />
+        </PostModelo>
     )
 }
