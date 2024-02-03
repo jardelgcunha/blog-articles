c85d1ba3625ec42a3f23625c9d1fd70afd3d20df
Jardel Cunha <jardelgcunha.ti@gmail.com>
2024-02-02 23:05:50 -0300

    Refactoring

--- src/paginas/Post/index.js
+++ src/paginas/Post/index.js
@@ -20,21 +20,18 @@ export default function Post() {
     console.log(post);
 
     return (
-        <Routes>
-            <Route path='*' element={<PaginaPadrao />}>
-                <Route index element={
-                    <PostModelo
-                        fotoCapa={`/assets/posts/${post.id}/capa.png`}
-                        titulo={post.titulo}
-                    >
-                        <div className="post-markdown-container">
-                            <ReactMarkdown>
-                                {post.texto}
-                            </ReactMarkdown>
-                        </div>
-                    </PostModelo>
-                } />
-            </Route>
-        </Routes>
+        <PaginaPadrao>
+            <PostModelo
+                fotoCapa={`/assets/posts/${post.id}/capa.png`}
+                titulo={post.titulo}
+            >
+                <div className="post-markdown-container">
+                    <ReactMarkdown>
+                        {post.texto}
+                    </ReactMarkdown>
+                </div>
+            </PostModelo>
+        </PaginaPadrao>
+
     )
 }
